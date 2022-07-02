29a1e9261e20aaebc56fcc3313623b00b5e64065
KevInZhao <hellozhaowenkai@gmail.com>
2022-07-02 22:10:53 +0800

    build(root): use Docker bridge networks

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
+# Delete container & image
 docker container rm -f about-me
-
 docker image rm about-me:latest
 
-docker image build --tag=about-me:latest .
+# Build image
+docker image build --tag=about-me:latest $PWD
 
+# Run container
 docker container run \
   --user     $(id -u) \
   --name     about-me \
+  --network  lab-net \
   --publish  10007:8888 \
   --volume   $PWD/nginx.conf:/etc/nginx/nginx.conf \
   --volume   $PWD/logs:/app/logs \
@@ -17,4 +20,5 @@ docker container run \
   --detach \
   about-me:latest
 
+# View log
 docker container logs about-me
